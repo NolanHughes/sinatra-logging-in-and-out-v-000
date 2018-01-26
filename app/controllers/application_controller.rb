e8cd743d5a1ab7a5a8efa1c77c4699d4113dede0
Nolan Hughes <nhughes987@gmail.com>
2018-01-26 16:53:42 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    if params[:username]
+    if params[:username] != nil
       @user = User.find_by(username: params[:username])
       session[:user_id] = @user[:id]
 
