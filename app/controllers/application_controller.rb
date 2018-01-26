feb68bfefce2bd178f2cf1d014ec9cb4a163057a
Nolan Hughes <nhughes987@gmail.com>
2018-01-26 15:54:30 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,9 +14,9 @@ class ApplicationController < Sinatra::Base
     @user = User.find_by(username: params[:username])
     if @user.id
       session[:user_id] = @user[:id]
-      redirect to '/account'
+      # redirect to '/account'
     else
-      erb :'error'
+      # erb :'error'
     end
   end
 
