c4846ca3e855b51c69aef8e9e25a80d4f026f22e
Nolan Hughes <nhughes987@gmail.com>
2018-01-26 15:48:39 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
 
   def initialize(username, password, balance)
     @username, @password, @balance = username, password, balance
+    binding.pry
     # self.save
   end
 end
