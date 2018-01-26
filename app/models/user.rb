576180228874f90dc83ef0e4a80974cbf6f9ab71
Nolan Hughes <nhughes987@gmail.com>
2018-01-26 15:49:54 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,6 @@ class User < ActiveRecord::Base
   attr_accessor :username, :password, :balance
 
   def initialize(username, password, balance)
-    # @username, @password, @balance = username, password, balance
-    binding.pry
-    # self.save
+    @username, @password, @balance = username, password, balance
   end
 end
